e04fdca6ecfecddab27465d0c3563f792245213c
chenna1292 <chennakesava1292@gmail.com>
2024-07-10 15:34:27 +0530

    Create a new class under first package

---
+++ src/firstPackage/WelcomeProgram.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class WelcomeProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome java");
+
+	}
+
+}
