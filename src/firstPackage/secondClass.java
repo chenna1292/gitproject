69cd1b02557b8d362a50fa07bd5bf054c65c8d37
chenna1292 <chennakesava1292@gmail.com>
2024-07-10 16:56:03 +0530

    added the line in second class

--- src/firstPackage/secondClass.java
+++ src/firstPackage/secondClass.java
@@ -5,6 +5,7 @@ public class secondClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Second program in java");
+		System.out.println("Added second line");
 
 	}
 
