57ac1d09be673187e686de206e38358498d82689
chenna1292 <chennakesava1292@gmail.com>
2024-07-10 15:47:26 +0530

    added second file under first package

---
+++ src/firstPackage/secondClass.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class secondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second program in java");
+
+	}
+
+}
